451606fc05914a612605d72d33cc5a2ad5668059
DonQuixoteI <don-quixote@gmx.net>
2019-02-22 17:26:12 +0100

    Removed dom.idle-observers-api.enabled

--- user-debian.js
+++ user-debian.js
@@ -91,7 +91,6 @@ user_pref("dom.enable_resource_timing", false);
 user_pref("dom.event.clipboardevents.enabled", false);
 user_pref("dom.event.contextmenu.enabled", false);
 user_pref("dom.gamepad.enabled", false);
-user_pref("dom.idle-observers-api.enabled", false);
 user_pref("dom.ipc.plugins.flash.subprocess.crashreporter.enabled", false);
 user_pref("dom.ipc.plugins.reportCrashURL", false);
 user_pref("dom.popup_maximum", 3);

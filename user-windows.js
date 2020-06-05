0a9635daaed5c1208d05610fd7a3a838afcfd551
DonQuixoteI <don-quixote@gmx.net>
2020-06-05 12:04:55 +0200

    Removed browser.newtabpage.activity-stream.showTopSites deprecated in FF61

--- user-windows.js
+++ user-windows.js
@@ -35,7 +35,6 @@ user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket",
 user_pref("browser.newtabpage.activity-stream.section.topstories.showDisclaimer", false);
 user_pref("browser.newtabpage.activity-stream.showSearch", false);
 user_pref("browser.newtabpage.activity-stream.showSponsored", false);
-user_pref("browser.newtabpage.activity-stream.showTopSites", false);
 user_pref("browser.newtabpage.activity-stream.telemetry", false);
 user_pref("browser.newtabpage.enabled", false);
 user_pref("browser.newtabpage.enhanced", false);

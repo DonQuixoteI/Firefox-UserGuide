b7e9426a8daf435fa88674a9acc75e538ad143d5
DonQuixoteI <don-quixote@gmx.net>
2020-07-19 21:29:12 +0200

    Removed browser.urlbar.oneOffSearches deprecated in FF77

--- user-windows.js
+++ user-windows.js
@@ -87,7 +87,6 @@ user_pref("browser.tabs.crashReporting.sendReport", false);
 user_pref("browser.tabs.loadInBackground", false);
 user_pref("browser.uitour.enabled", false);
 user_pref("browser.uitour.url", "");
-user_pref("browser.urlbar.oneOffSearches", false);
 user_pref("browser.urlbar.searchSuggestionsChoice", false);
 user_pref("browser.urlbar.speculativeConnect.enabled", false);
 user_pref("browser.urlbar.suggest.history", false);
